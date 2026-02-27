a00703d47ce9a6153de5729e7e52c8a0ae5b2f83
Ullas K P <kpullasgowda11@gmail.com>
2026-02-27 22:20:02 +0530

    Added Day 07

---
+++ Day_07/Functions with Inputs.py
@@ -0,0 +1,17 @@
+#def greet():
+    #print("hey hii ")
+    #print("whats upp!")
+    #print("?")
+
+
+#greet()
+
+def greet_with_me(name):
+    print(f"Hello {name}")
+    print(f"How Do you Do {name}")
+    print("?")
+
+
+greet_with_me("ullas")
+
+
---
+++ Day_07/Positional Vs Keyword Arguments.py
@@ -0,0 +1,10 @@
+# Functions with input
+
+def greet_with_name(name,location):
+    print(f"Hello {name}")
+    print(f"what is like in {location}?")
+
+
+#greet_with_name("Jack Bauer","japan")
+
+greet_with_name(location="japan", name="Jack Bauer" )
